e7baa99c587d173338527869fc8bb2062669413d
Maria Kirilova <47693680+mariakirilova2004@users.noreply.github.com>
2022-02-15 21:41:38 +0200

    Add files via upload

---
+++ Keep_Notes/Keep_Notes/Migrations/20220214073242_Adding creation_date and latest_date in Notes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.Data.EntityFrameworkCore.Metadata;
+
+namespace Keep_Notes.Migrations
+{
+    public partial class Addingcreation_dateandlatest_dateinNotes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
---
+++ Keep_Notes/Keep_Notes/Model/Notes.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace Keep_Notes.Model
+{
+    public partial class Notes
+    {
+        public string Title { get; set; }
+        [Column]
+        public DateTime creation_date { get; set; }
+        [Column]
+        public DateTime latest_change { get; set; }
+        public string Note { get; set; }
+        public bool Private { get; set; }
+        public string Password { get; set; }
+        public string Keywords { get; set; }
+        public string Color { get; set; }
+        public int UserId { get; set; }
+
+        public virtual Users User { get; set; }
+    }
+}
